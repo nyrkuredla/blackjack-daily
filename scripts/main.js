024efc96bfbdbf4028d7b29d2bfef27b4015aed5
Ryan Kelley <rtkelley@gmail.com>
2017-08-02 17:29:17 -0400

    done, need to add comments

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,81 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
+  let handTotal = 0;
+  let acesArr = [];
 
+  function isFace (card) {
+    if ((card === "J") || (card === "Q") || (card === "K")) {
+      return true;
+    }
+  }
+  function isAce (card) {
+    if (card === "A") {
+      return true;
+    }
+  }
+  function isNumber (card) {
+    if ((card !== "K") && (card !== "Q") && (card !== "J") && (card !== "A")) {
+      return true;
+    }
+  }
 
-  return;
+  function numberValue (card) {
+    if (card === "2") {
+      handTotal += 2;
+    }
+    else if (card === "3") {
+      handTotal += 3;
+    }
+    else if (card === "4") {
+      handTotal += 4;
+    }
+    else if (card === "5") {
+      handTotal += 5;
+    }
+    else if (card === "6") {
+      handTotal += 6;
+    }
+    else if (card === "7") {
+      handTotal += 7;
+    }
+    else if (card === "8") {
+      handTotal += 8;
+    }
+    else if (card === "9") {
+      handTotal += 9;
+    }
+  }
+
+  hand.forEach(function (item) {
+    if (isFace(item) === true) {
+      handTotal += 10;
+    }
+    else if (isNumber(item) === true) {
+      numberValue(item);
+    }
+    else if (isAce(item) === true) {
+      acesArr.push("ace!!");
+    }
+
+    });
+
+    for (let i = 0; i < acesArr.length; i++) {
+      if (handTotal >= 11) {
+        handTotal += 1;
+      }
+      else {
+        handTotal += 11;
+      }
+    }
+
+  return handTotal;
 }
 
 
